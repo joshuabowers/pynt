6d9d8e193bb40eb4e705d8f619a12b732475a301
Joshua Bowers <joshua.bowers+code@gmail.com>
2012-07-22 11:03:24 -0700

    More UI tweaks. Along with a few alterations to make terminal work again.

--- app/controllers/terminal_controller.rb
+++ app/controllers/terminal_controller.rb
@@ -1,10 +1,6 @@
 class TerminalController < ApplicationController
   before_filter :authenticate_user!
   
-  # Essentially, should do something like:
-  # @game = Game.where(title: params['title'])
-  # @current_state = @game.load_last_save_for(current_user)
-  # which would return a State object, to populate the terminal
   def index
     @game = Game.where(parameterized_title: params['title']).first
     if @game
@@ -14,9 +10,6 @@ class TerminalController < ApplicationController
     end
   end
 
-  # Again, like the above, this would be something like:
-  # @game = Game.find(params['game_id'])
-  # @current_state = @game.execute(@command_line)
   def execute
     @game = Game.find(params['game_id'])
     @command_line = params['command_line']
