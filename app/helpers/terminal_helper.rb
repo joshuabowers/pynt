6bceed9454879bd018bd6b225b2919e53150b528
Joshua Bowers <joshua.bowers+code@gmail.com>
2012-07-24 11:46:27 -0700

    Game objects are now highlighted in different colors to make them stand out more. This is only performed for objects which are wrapped in brackets in some description.

--- app/helpers/terminal_helper.rb
+++ app/helpers/terminal_helper.rb
@@ -1,2 +1,8 @@
 module TerminalHelper
+  def mark_up_description(game_state)
+    game_state.description.gsub(/\[([^\]]+)\]/) do |s|
+      object = game_state.game_save.current_room.objects.where(name: $1).first
+      content_tag(:span, $1, class: object.class.name.underscore)
+    end.html_safe
+  end
 end
